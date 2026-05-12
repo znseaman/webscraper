b064447061010518a9428d1e9cb383b2137a1ab2
znseaman <znseaman@gmail.com>
2026-05-12 16:56:02 -0700

    feat: accept url from command line

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import { argv } from 'node:process';
+
+function main() {
+	if (argv.length !== 3) {
+		console.error(`usage: npm start <BASE_URL>`)
+		process.exit(1)
+	}
+
+	const baseURL = argv[2]
+	console.log(`Starting crawler from ${baseURL}...`)
+}
+
+main();
