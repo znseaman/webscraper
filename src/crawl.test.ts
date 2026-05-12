8e019bbbe228c16faf5b18c17d99ef70a8ea349b
znseaman <znseaman@gmail.com>
2026-05-12 14:58:57 -0700

    feat: add get heading and get first paragraph text content

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { normalizeURL } from './crawl.js'
+import { normalizeURL, getHeadingFromHTML, getFirstParagraphFromHTML } from './crawl.js'
 
 test('normalize 1st url: https://www.boot.dev/blog/path/', () => {
   const url = 'https://www.boot.dev/blog/path/'
@@ -24,4 +24,43 @@ test('normalize 4th url: http://www.boot.dev/blog/path', () => {
   const expected = 'www.boot.dev/blog/path'
   expect(normalizeURL(url)).toBe(expected)
 })
+
+test('get heading from html: has h1, return text content', () => {
+  const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+  const expected = 'Test Title'
+  expect(getHeadingFromHTML(inputBody)).toBe(expected)
+})
+
+test('get heading from html: has h2, return text content', () => {
+  const inputBody = '<html><body><h2>Test Title 2</h2></body></html>'
+  const expected = 'Test Title 2'
+  expect(getHeadingFromHTML(inputBody)).toBe(expected)
+})
+
+test('get heading from html: no h1 or h2 found, return empty string', () => {
+  const inputBody = '<html><body><p>No return</p></body></html>'
+  const expected = ''
+  expect(getHeadingFromHTML(inputBody)).toBe(expected)
+})
+
+test('get first paragraph from html: p found, return text content', () => {
+  const inputBody = '<html><body><p>Test Paragraph</p></body></html>'
+  const expected = 'Test Paragraph'
+  expect(getFirstParagraphFromHTML(inputBody)).toBe(expected)
+})
+
+test('get first paragraph from html: no p found, return empty string', () => {
+  const inputBody = '<html><body><a>Yeah!</p></body></html>'
+  const expected = ''
+  expect(getFirstParagraphFromHTML(inputBody)).toBe(expected)
+})
+
+test('get first paragraph from html: main found, return text content', () => {
+  const inputBody = '<html><body><main>Main Section</main></body></html>'
+  const expected = 'Main Section'
+  expect(getFirstParagraphFromHTML(inputBody)).toBe(expected)
+})
+
+
+
 
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from 'jsdom'
+
 export function normalizeURL(url: string): string {
 	let {hostname, pathname} = new URL(url)
 
@@ -8,3 +10,34 @@ export function normalizeURL(url: string): string {
 
 	return `${hostname}${pathname}`
 }
+
+export function getHeadingFromHTML(html: string): string {
+	const dom = new JSDOM(html)
+	const h1 = dom.window.document.querySelectorAll('h1')
+	if (h1.length > 0) {
+		return h1[0].textContent
+	}
+	
+	const h2 = dom.window.document.querySelectorAll('h2')
+	if (h2.length > 0) {
+		return h2[0].textContent
+	}
+
+	return ''
+}
+
+export function getFirstParagraphFromHTML(html: string): string {
+	const dom = new JSDOM(html)
+	const main = dom.window.document.querySelectorAll('main')
+	if (main.length > 0) {
+		return main[0].textContent
+	}
+
+	const p = dom.window.document.querySelectorAll('p')
+	if (p.length > 0) {
+		return p[0].textContent
+	}
+
+	return ''
+}
+
